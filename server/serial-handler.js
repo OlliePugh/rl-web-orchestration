6611a729c0245c9606f6eb31b51b09cf69f73648
Ollie Pugh <oliver.pugh@mac.com>
2022-04-17 21:36:44 +0100

    Added buffer to serial coms

--- server/serial-handler.js
+++ server/serial-handler.js
@@ -1,11 +1,29 @@
+let lastControlDispatchTime = 0;
+let lastControlDispatchState;
+let retryTimeout;
+const controlDispatchMinFreq = 25; // every 25 ms
+
 const serialHandler = () => {
   console.log("Serial Called");
 };
 
-const dispatchControlState = async (port, controlState) => {
+const dispatchControlState = async (port, controlState, force) => {
+  lastControlDispatchState = controlState;
+  if (!force && Date.now() < lastControlDispatchTime + controlDispatchMinFreq) {
+    // has not been long enough since last send
+    if (!retryTimeout) {
+      retryTimeout = setTimeout(() => {
+        dispatchControlState(port, lastControlDispatchState);
+      }, controlDispatchMinFreq - (Date.now() - lastControlDispatchTime));
+    }
+    return; // exit early
+  }
   try {
+    clearTimeout(retryTimeout);
+    retryTimeout = null;
     const toSend = JSON.stringify(controlState);
     console.log(`writing to arena: ${toSend}`);
+    lastControlDispatchTime = Date.now();
     if (port.isOpen()) {
       await port.write(toSend);
     } else {
